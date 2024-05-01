10c20066a86961681ab78e3dc6d4e67f9b703eec
aminoxlhd <aminoxlhd@gmail.com>
2024-05-01 23:33:32 +0000

    task 6

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -44,4 +44,16 @@ function createEmployee(salary: number | string): Director | Teacher {
   } else {
     return new Director();
   }
+  }
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return (employee as Director).workDirectorTasks !== undefined;
+  }
+
+function executeWork(employee: Director | Teacher): string {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
 }
