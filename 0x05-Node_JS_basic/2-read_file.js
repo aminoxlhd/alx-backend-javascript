1b16be500a72244ac6145065ff97976e78b8da92
aminoxlhd <aminoxlhd@gmail.com>
2024-06-19 02:49:43 +0100

    edit Task 2

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,28 +1,30 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 
-function countStudents (filePath) {
-  try {
-    const data = fs.readFileSync(filePath, 'utf-8').split('\n');
-    const students = [];
-    const fields = {};
+function countStudents (path) {
+  return fs.readFile(path, 'utf-8')
+    .then((data) => {
+      const students = [];
+      const fields = {};
 
-    data.shift();
+      const lines = data.split('\n').slice(1);
 
-    for (const line of data) {
-      if (line.trim() === '') continue;
-      const [firstName, , , field] = line.split(',');
-      students.push(firstName);
-      fields[field] = (fields[field] || 0) + 1;
-    }
+      for (const line of lines) {
+        if (line.trim() === '') continue;
+        const [firstName, , , field] = line.split(',');
+        students.push(firstName);
+        fields[field] = (fields[field] || 0) + 1;
+      }
 
-    console.log(`Number of students: ${students.length}`);
+      console.log(`Number of students: ${students.length}`);
 
-    for (const field in fields) {
-      console.log(`Number of students in ${field}: ${fields[field]}. List: ${students.filter((name) => line.includes(`${name},${field}`)).join(', ')}`);
-    }
-  } catch (error) {
-    throw new Error('Cannot load the database');
-  }
+      for (const field in fields) {
+        const filteredStudents = students.filter((name) => line.includes(`${name},${field}`));
+        console.log(`Number of students in ${field}: ${fields[field]}. List: ${filteredStudents.join(', ')}`);
+      }
+    })
+    .catch((error) => {
+      throw new Error('Cannot load the database');
+    });
 }
 
 module.exports = countStudents;
