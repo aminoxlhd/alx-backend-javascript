b8a3d4abfe0bf783a27bad27d320d7663e5bf2fc
aminoxlhd <aminoxlhd@gmail.com>
2024-06-22 02:54:36 +0100

    edit task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -1,31 +1,63 @@
 const express = require('express');
-const countStudents = require('./3-read_file_async');
+
+const { readFile } = require('fs');
 
 const app = express();
 const port = 1245;
 
-app.get('/', (req, res) => {
-  res.type('text/plain');
-  res.send('Hello Holberton School!');
-});
+function countStudents (fileName) {
+  const students = {};
+  const fields = {};
+  let length = 0;
+  return new Promise((resolve, reject) => {
+    readFile(fileName, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        let output = '';
+        const lines = data.toString().split('\n');
+        for (let i = 0; i < lines.length; i += 1) {
+          if (lines[i]) {
+            length += 1;
+            const field = lines[i].toString().split(',');
+            if (Object.prototype.hasOwnProperty.call(students, field[3])) {
+              students[field[3]].push(field[0]);
+            } else {
+              students[field[3]] = [field[0]];
+            }
+            if (Object.prototype.hasOwnProperty.call(fields, field[3])) {
+              fields[field[3]] += 1;
+            } else {
+              fields[field[3]] = 1;
+            }
+          }
+        }
+        const l = length - 1;
+        output += `Number of students: ${l}\n`;
+        for (const [key, value] of Object.entries(fields)) {
+          if (key !== 'field') {
+            output += `Number of students in ${key}: ${value}. `;
+            output += `List: ${students[key].join(', ')}\n`;
+          }
+        }
+        resolve(output);
+      }
+    });
+  });
+}
 
-app.get('/students', async (req, res) => {
-  try {
-    const filePath = process.argv[2];
-    const data = await countStudents(filePath);
-    res.type('text/plain');
-    res.send(`This is the list of our students\n${data}`);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
+app.get('/', (request, response) => {
+  response.send('Hello Holberton School!');
 });
-
-app.get('*', (req, res) => {
-  res.status(404).send('Not found');
+app.get('/students', (request, response) => {
+  countStudents(process.argv[2].toString()).then((output) => {
+    response.send(['This is the list of our students', output].join('\n'));
+  }).catch(() => {
+    response.send('This is the list of our students\nCannot load the database');
+  });
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}/`);
 });
 
 module.exports = app;
