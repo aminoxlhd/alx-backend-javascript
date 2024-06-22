493c4aeecb1eb20b8b462e902f368c267fe338b7
aminoxlhd <aminoxlhd@gmail.com>
2024-06-22 02:52:30 +0100

    add task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -4,27 +4,24 @@ const countStudents = require('./3-read_file_async');
 const app = express();
 const port = 1245;
 
-// Handle the root path
 app.get('/', (req, res) => {
-  res.type('text/plain'); // Set response content type to plain text
+  res.type('text/plain');
   res.send('Hello Holberton School!');
 });
 
-// Handle the /students path
 app.get('/students', async (req, res) => {
   try {
-    const filePath = process.argv[2]; // Access database path from argument
+    const filePath = process.argv[2];
     const data = await countStudents(filePath);
-    res.type('text/plain'); // Set response content type to plain text
+    res.type('text/plain');
     res.send(`This is the list of our students\n${data}`);
   } catch (error) {
-    res.status(500).send(error.message); // Send error message on failure
+    res.status(500).send(error.message);
   }
 });
 
-// Handle all other paths (optional)
 app.get('*', (req, res) => {
-  res.status(404).send('Not found'); // Send 404 for unmatched routes
+  res.status(404).send('Not found');
 });
 
 app.listen(port, () => {
