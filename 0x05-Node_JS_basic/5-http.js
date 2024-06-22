39a6248fdecab1d47166e05bccf1d8db90ca6550
aminoxlhd <aminoxlhd@gmail.com>
2024-06-22 02:24:38 +0100

    add task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,31 @@
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const app = http.createServer((req, res) => {
+  const url = req.url;
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+  } else if (url === '/students') {
+    const filePath = process.argv[2]; // Access database path from argument
+    countStudents(filePath)
+      .then((data) => {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end(`This is the list of our students\n${data}`);
+      })
+      .catch((error) => {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end(error.message);
+      });
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not found');
+  }
+});
+
+const port = 1245;
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+module.exports = app;
