34ffada2abd57fc7382d73942309e14bbdc988fc
aminoxlhd <aminoxlhd@gmail.com>
2024-06-22 02:09:57 +0100

    edit task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,30 +1,38 @@
 const fs = require('fs').promises;
 
-function countStudents (path) {
-  return fs.readFile(path, 'utf-8')
-    .then((data) => {
-      const students = [];
-      const fields = {};
-
-      const lines = data.split('\n').slice(1);
-
-      for (const line of lines) {
-        if (line.trim() === '') continue;
-        const [firstName, , , field] = line.split(',');
-        students.push(firstName);
-        fields[field] = (fields[field] || 0) + 1;
-      }
-
-      console.log(`Number of students: ${students.length}`);
-
-      for (const field in fields) {
-        const filteredStudents = students.filter((name) => line.includes(`${name},${field}`));
-        console.log(`Number of students in ${field}: ${fields[field]}. List: ${filteredStudents.join(', ')}`);
-      }
-    })
-    .catch((error) => {
-      throw new Error('Cannot load the database');
-    });
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8')
+      .then((data) => {
+        const lines = data.split('\n');
+        const hashtable = {};
+        let students = -1;
+        for (const line of lines) {
+          if (line.trim() !== '') {
+            const columns = line.split(',');
+            const field = columns[3];
+            const firstname = columns[0];
+            if (students >= 0) {
+              if (!Object.hasOwnProperty.call(hashtable, field)) {
+                hashtable[field] = [];
+              }
+              hashtable[field] = [...hashtable[field], firstname];
+            }
+            students += 1;
+          }
+        }
+        console.log(`Number of students: ${students}`);
+        for (const key in hashtable) {
+          if (Object.hasOwnProperty.call(hashtable, key)) {
+            console.log(`Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}`);
+          }
+        }
+        resolve();
+      })
+      .catch(() => {
+        reject(new Error('Cannot load the database'));
+      });
+  });
 }
 
 module.exports = countStudents;
